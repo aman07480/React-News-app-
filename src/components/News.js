a055e83169bdea6c09d3698849a415c8d162d146
aman tamboli 07 <tamboliaman109@gmail.com>
2026-06-01 18:14:47 +0530

    created news react app

---
+++ src/components/News.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// import Newsitem from './components/Newsitem';
+import Newsitem from './Newsitem'
+export class news extends Component {
+  render() {
+    return (
+      <div>
+        news component
+        <Newsitem/>
+      </div>
+    )
+  }
+}
+
+export default news
