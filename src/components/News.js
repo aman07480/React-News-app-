d5e36b86a2d72764a8374c654597bb36872c0245
aman tamboli 07 <tamboliaman109@gmail.com>
2026-06-05 16:02:30 +0530

    update in some code

--- src/components/News.js
+++ src/components/News.js
@@ -6,8 +6,21 @@ export class news extends Component {
     return (
       <div className="container my-3">
         <h2>News Monkey -Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+               <newsitem  title="myTitle" description='mydesc' imageUrl="https://www.dreamstime.com/striking-silhouette-two-cricket-players-protective-gear-standing-ready-stance-holding-cricket-bats-high-poised-image437356421"/>
+          </div>
+
+          <div className="col-md-4">
+                <newsitem  title="myTitle" description='mydesc'/>
+          </div>
+
+          <div className="col-md-4">
+                <newsitem  title="myTitle" description='mydesc'/>
+          </div>
+      </div>
         
-        <newsitem  title='myTitle' description='mydesc' />
+        <Newsitem/>
         <Newsitem/>
       </div>
     )
