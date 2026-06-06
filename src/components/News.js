fe35ba8c230091b57d19e102dc269dd90d8dbb17
aman tamboli 07 <tamboliaman109@gmail.com>
2026-06-06 20:22:06 +0530

    change in title

--- src/components/News.js
+++ src/components/News.js
@@ -5,7 +5,7 @@ export class news extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h2>News Monkey -Top Headlines</h2>
+        <h2>News Monkey-Top Headlines</h2>
         <div className="row">
           <div className="col-md-4">
                <newsitem  title="myTitle" description='mydesc' imageUrl="https://www.dreamstime.com/striking-silhouette-two-cricket-players-protective-gear-standing-ready-stance-holding-cricket-bats-high-poised-image437356421"/>
